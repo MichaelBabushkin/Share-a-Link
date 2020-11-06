2139f7e2cc2d600daac3a23a3c4b0cf8675d935b
Michael <mishaba1990@gmail.com>
2020-11-06 17:58:20 +0200

    server updated

--- server/server.js
+++ server/server.js
@@ -2,32 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const path = require('path');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 
-//Serve up static assets (heroku)
-// if (process.env.NODE_ENV=== "production"){
-//   app.use(express.static("client/build"));
-// }
-//
-
-// If in production, then use static frontend build files.
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri||'mongodb+srv://Michael:Mishaba123@cluster0.cxn1c.gcp.mongodb.net/links?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true }
@@ -43,6 +29,15 @@ const usersRouter = require('./routes/users');
 app.use('/links', linksRouter);
 app.use('/users', usersRouter);
 
+//Serve up static assets (heroku)
+if(process.env.NODE_ENV === "development"){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
