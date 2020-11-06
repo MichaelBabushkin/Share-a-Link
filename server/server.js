5d8773144434c01a0ea2ee366ee86c335717695c
Michael <mishaba1990@gmail.com>
2020-11-06 19:08:05 +0200

    production

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use('/links', linksRouter);
 app.use('/users', usersRouter);
 
 //Serve up static assets (heroku)
-if(process.env.NODE_ENV === "development"){
+if(process.env.NODE_ENV === "production"){
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
