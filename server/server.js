ebc05749055e7baa2d5d62b6300e9ec479619d1f
Michael <mishaba1990@gmail.com>
2020-11-06 15:09:07 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+const path = require('path');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,10 +13,21 @@ app.use(cors());
 app.use(express.json());
 
 //Serve up static assets (heroku)
-if (process.env.NODE_ENV=== "production"){
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV=== "production"){
+//   app.use(express.static("client/build"));
+// }
 //
+
+// If in production, then use static frontend build files.
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
