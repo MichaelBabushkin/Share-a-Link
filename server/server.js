9155969ec0658855d7aaff9bc0b12a71ba669bf4
Michael <mishaba1990@gmail.com>
2020-11-07 14:25:50 +0200

    expressless

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json());
+// app.use(express.json());
 
 
 const uri = process.env.ATLAS_URI;
@@ -34,7 +34,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
+  });
 }
 
 const port = process.env.PORT || 5000;
